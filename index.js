61d3682e01eb9a334a84e26d779bce0383b7bacd
rotemetreex <60839395+rotemetreex@users.noreply.github.com>
2021-08-06 22:40:39 +0300

    added mouse and touch functionality in js

--- index.js
+++ index.js
@@ -1,4 +1,62 @@
 var isHidden = true;
+var chosenColorfromPalete;
+
+window.addEventListener('load', () => {
+    const canvas = document.querySelector('#canvas');
+    const ctx = canvas.getContext('2d');
+
+    canvas.height = window.innerHeight /2;
+    canvas.width = window.innerWidth -200;
+
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+    // canvas.height = 150;
+    // canvas.width = 300;
+
+
+    let painting = false;
+
+    function startPosition(e){
+        painting = true;
+        draw(e);
+    }
+
+    function finishPosition(){
+        painting = false;
+        ctx.beginPath();
+    }
+
+    function draw(e) {
+        if(!painting) return;
+
+        ctx.lineWidth = 3;
+        ctx.strokeStyle = chosenColorfromPalete;
+        ctx.lineCap = 'round';
+
+        ctx.lineTo(e.clientX - canvas.offsetLeft, e.clientY);
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.moveTo(e.clientX - canvas.offsetLeft, e.clientY);
+
+    }
+
+    canvas.addEventListener('mousedown', startPosition);
+    canvas.addEventListener('touchstart', startPosition);
+    canvas.addEventListener('mouseup', finishPosition);
+    canvas.addEventListener('touchend', finishPosition);
+    canvas.addEventListener('mousemove', draw);
+    canvas.addEventListener('touchmove', draw);
+
+
+    document.querySelector('#ulColorPalete').addEventListener('click', (event) => {
+        // chosenColorfromPalete = event.target.style.backgroundColor;
+        var computedStyle = getComputedStyle(event.target);
+        chosenColorfromPalete = computedStyle.getPropertyValue('background-color');
+    });
+
+});
+
 
 
 function getColorValuesById(elementId) {
@@ -16,7 +74,7 @@ function getColorValuesAsRGB() {
 
     document.getElementById('preview').style.backgroundColor = rgbColor;
 
-    console.log(rgbColor);
+    // console.log(rgbColor);
 
     return rgbColor;
 }
@@ -41,12 +99,12 @@ function createColorFromRgbValue() {
 
     var newColor = document.getElementById('preview').style.backgroundColor;
     var ulElement = document.getElementById('ulColorPalete');
-
     var liElement = document.createElement('li');
-    liElement.setAttribute('id', 'newChosenColor');
-    liElement.setAttribute('class', 'colorCircle');
-    liElement.setAttribute('style', 'background-color');
+    liElement.classList.add('colorCircle');
     liElement.style.backgroundColor = newColor;
     ulElement.appendChild(liElement);
 
 }
+
+
+
